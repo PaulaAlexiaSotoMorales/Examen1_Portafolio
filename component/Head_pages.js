d57cae3313f912b7660b163a608a211aac45063f
PaulaAlexiaSotoMorales <130626285+PaulaAlexiaSotoMorales@users.noreply.github.com>
2023-05-08 00:10:44 -0600

    Create Head_pages.js

---
+++ component/Head_pages.js
@@ -0,0 +1,16 @@
+
+
+const Head_pages = ({ }) => {
+    return (
+        <nav className="etiqueta">
+
+
+            <div className="contain">Portafolio
+                <img className="logo" src="/img1.jpg" alt="not found" />
+
+            </div>
+        </nav>
+
+    )
+}
+export default Head_pages
