94722fd86c3adf4e20dd3835e6c2d24207236ab3
PaulaAlexiaSotoMorales <130626285+PaulaAlexiaSotoMorales@users.noreply.github.com>
2023-05-08 00:12:25 -0600

    Create Information.js

---
+++ component/Information.js
@@ -0,0 +1,26 @@
+const PerfilU = ({ }) => {
+    return (
+
+       <>
+
+        <div className="perr">
+            <h1 className="title">Personal information</h1>
+            <img className="images" src="/PM.jpeg" alt="Not Found" />
+
+            <h1 className="info">Paula Alexia Soto Morales</h1>
+            
+            <div className="carrera">Information Systems Engineer</div>
+
+            <div className="defi">
+                I feel qualified to apply for the job available at Empresa Actualidad.
+                The challenges and tasks that I have to face in your institution will be
+                completed professionally and with the greatest possible efficiency.
+
+            </div>
+        
+        </div>
+        <div className="tit"></div>
+        </>
+    )
+}
+export default PerfilU
