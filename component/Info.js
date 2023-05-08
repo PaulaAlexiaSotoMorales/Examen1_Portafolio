691a0e059e645bea0eebe73707e2efcb11139576
PaulaAlexiaSotoMorales <130626285+PaulaAlexiaSotoMorales@users.noreply.github.com>
2023-05-08 00:11:33 -0600

    Create Info.js

---
+++ component/Info.js
@@ -0,0 +1,13 @@
+const info = ({ }) => {
+    return (
+        <div className="abajo">
+
+
+            <h2 className="contact">Contactar:(85089343)</h2>
+            <h6 className="copy">Copyright ® 2023</h6>
+            <img className="p" src="/img1.jpg" alt="not found" />
+        </div>
+    )
+}
+
+export default info
